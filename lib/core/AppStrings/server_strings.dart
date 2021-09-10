36d7587d7ab1d3c590a9c45c83ec433d12f90363
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-10 15:04:04 +0800

    no message

--- lib/core/AppStrings/server_strings.dart
+++
@@ -1,7 +0,0 @@
-const Basic = "/basic";
-const BaseURL = "https://api.nytimes.com/svc";
-const BaseURLSTAGING = "https://api.nytimes.com/svc";
-const Version = "/v2";
-//TODO : please get yours from the developer sit of NYT
-const ApiKey = "";
-const ApiKeyStaging = "";
