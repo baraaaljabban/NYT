5585d91b71bbe5efa26668a09e556bd3533ebb44
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-10 14:54:05 +0800

    Add Remote Data Source Layer and network Connection Class Helper

---
+++ lib/core/AppStrings/server_strings.dart
@@ -0,0 +1,7 @@
+const Basic = "/basic";
+const BaseURL = "https://api.nytimes.com/svc";
+const BaseURLSTAGING = "https://api.nytimes.com/svc";
+const Version = "/v2";
+//TODO : please get yours from the developer sit of NYT
+const ApiKey = "";
+const ApiKeyStaging = "";
