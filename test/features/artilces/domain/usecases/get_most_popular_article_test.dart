e54c05c3bf5c2ac2f462c7e7495e5f189946b423
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-12 14:42:48 +0800

    testing Most popular emailed articles

--- test/features/artilces/domain/usecases/get_most_popular_article_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mockito/mockito.dart';
-import 'package:nyt_app/features/articles/article_export/articl_export.dart';
-import 'package:flutter_test/flutter_test.dart';
-class MockArticleRepository extends Mock implements  ArticleRepository{
-
-}
