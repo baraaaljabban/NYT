ae191b87a9b4bc9041f480e0a33e0090d5b69778
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-13 14:57:00 +0800

    add App Strings

--- lib/features/articles/presentation/controllers/article_list_controller.dart
+++ lib/features/articles/presentation/controllers/article_list_controller.dart
@@ -16,22 +16,25 @@ class ArticleListController extends StatelessWidget {
       ),
       body: articles.length == 0
           ? Text(NO_DATA_FOUND)
-          : ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: articles.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  borderOnForeground: true,
-                  clipBehavior: Clip.hardEdge,
-                  elevation: 5,
-                  child: ListTile(
-                    leading: Text("${index+1}"),
-                    title: Text(articles[index].title),
-                    subtitle: Text(articles[index].publishedDate),
-                  ),
-                );
-              },
-            ),
+          : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: articles.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    borderOnForeground: true,
+                    clipBehavior: Clip.hardEdge,
+                    elevation: 5,
+                    child: ListTile(
+                      leading: Text("${index+1}"),
+                      title: Text(articles[index].title),
+                      subtitle: Text(articles[index].publishedDate),
+                    ),
+                  );
+                },
+              ),
+          ),
     );
   }
 }
